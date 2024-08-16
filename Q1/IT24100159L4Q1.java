6d3fd787d14c98fc9f27b56a9acf258bb5e85fd4
IT24100159 <it24100159@my.sliit.lk>
2024-08-16 11:29:46 +0530

    Add IT24100159L4Q1.java to Q1

---
+++ Q1/IT24100159L4Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IT24100159L4Q1{
+ public static void main(String[]args){
+ int num1;
+ Scanner scanner = new Scanner(System.in);
+ System.out.println("Enter a number");
+ num1 = scanner.nextInt();
+ if(num1>0){
+  System.out.print("The number is: Positive");
+ } else if (num1<0){
+  System.out.print("The number is: Negative");
+}else {
+  System.out.print("The number is Zero");
+   }
+}
+}
+  
+
+  
